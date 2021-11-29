4b0a3c2396909d45a8967637ec76ea644d7ca574
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-11-29 06:20:30 -0500

    Too see changes, check the Test Scenes

--- Final Unity Project_696/Assets/Scripts/PickUp.cs
+++ Final Unity Project_696/Assets/Scripts/PickUp.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    public Transform destination;
-    private Rigidbody rb; 
+    private Transform destination;
+    private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); 
+        destination = GameObject.Find("Destination").transform; 
     }
-    
+
     private void OnMouseDown()
     {
-        if (gameObject.tag == "interactable")
-        {
-            GetComponent<Collider>().enabled = false; 
-            rb.useGravity = false;
-            transform.position = destination.position;
-            transform.parent = GameObject.Find("Destination").transform;
-            transform.rotation = new Quaternion(0, 0, 0, 0);
-            rb.velocity = new Vector3(0, 0, 0);
-            //rb.isKinematic = true; 
-        }
+        //if (gameObject.tag == "interactable")
+        GetComponent<Collider>().enabled = false; 
+        rb.useGravity = false;
+        transform.position = destination.position;
+        transform.parent = destination;
+        transform.rotation = new Quaternion(0, 0, 0, 0);
+        rb.velocity = new Vector3(0, 0, 0);
+        //rb.isKinematic = true; 
     }
 
     private void OnMouseUp()
     {
-        GetComponent<Collider>().enabled = false;
+        GetComponent<Collider>().enabled = true;
         this.transform.parent = null;
         //rb.isKinematic = false;  
         rb.useGravity = true; 
